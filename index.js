3982f21b24938dca219b67188dbec45347ff15de
adarvin14 <amdarvin14@gmail.com>
2021-02-15 15:12:24 -0500

    Done

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = time.split(':')[0]
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour < 18) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  const greeting = document.getElementById('greeting');
+  greeting.innerText = string;
+}
